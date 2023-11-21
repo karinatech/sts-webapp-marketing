42a098c0bf85794886587221c8ab58bd9bfa6cab
Vasyl Fomiuk <vasyl@cybertekschool.com>
2023-11-21 10:40:14 -0500

    fixed contact us message

--- xavro-ng/src/app/components/common/contact/contact.component.ts
+++ xavro-ng/src/app/components/common/contact/contact.component.ts
@@ -3,14 +3,27 @@ import {ViewportScroller} from '@angular/common';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {ContactUsService} from "../../../services/contact-us/contact-us.service";
 import {environment} from "../../../../environments/environment";
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
     selector: 'app-contact',
     templateUrl: './contact.component.html',
-    styleUrls: ['./contact.component.scss']
+    styleUrls: ['./contact.component.scss'],
+    animations: [
+        trigger('fade', [
+            state('void', style({ opacity: 0 })),
+            transition(':enter', [animate('0s')]),  // Immediately visible on enter
+            transition(':leave', [animate('5s')])  // Fades out over 5 seconds
+        ])
+    ]
 })
 export class ContactComponent implements OnInit {
     form: FormGroup;
+    successMessageText: string = `Thank you for contacting us! We've received your message and our team will get back to you shortly.`;
+    errorMessageText: string = `Oops! Something went wrong. Please try again later.`;
+    messageText: string;
+    showMessage: boolean;
+    success: boolean;
 
     constructor(private viewportScroller: ViewportScroller,
                 private contactUsService: ContactUsService
@@ -21,7 +34,6 @@ export class ContactComponent implements OnInit {
         this.form = new FormGroup({
             name: new FormControl('', [Validators.required]),
             email: new FormControl('', [Validators.required, Validators.email]),
-            description: new FormControl('', [Validators.required]),
             number: new FormControl('', [Validators.required]),
             subject: new FormControl('', [Validators.required]),
             message: new FormControl('', [Validators.required]),
@@ -29,6 +41,10 @@ export class ContactComponent implements OnInit {
     }
 
     onSubmit() {
+        if (this.form.invalid) {
+            this.displayMessage('error');
+            return;
+        }
         const formValue: Contact = this.form.value;
         const headers = new Headers();
         headers.append('Content-Type', 'Content-Type: application/json');
@@ -36,11 +52,21 @@ export class ContactComponent implements OnInit {
         this.contactUsService.create<any>(formValue, headers).subscribe((response: any) => {
             console.log(`response: ${JSON.stringify(response)}`);
             if (response) {
+                this.displayMessage();
                 this.form.reset();
             }
         }, error => {
+            this.success = false;
+            this.displayMessage('error');
             console.error(`error: ${JSON.stringify(error)}`);
         });
+    }
+
+    displayMessage(success: string = 'success') {
+        this.showMessage = true;
+        this.success = success === 'success';
+        this.messageText = success === 'success' ? this.successMessageText : this.errorMessageText;
+        setTimeout(() => this.showMessage = false, 5000);
     }
 
     public onClick(elementId: string): void {
